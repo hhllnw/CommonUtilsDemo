aa9a514f1430a4dcbdf0821d68d6df4c05301f68
hhllnw <hhllnw@gmail.com>
2021-03-12 15:01:16 +0800

    集合学习

--- app/src/main/java/com/hhl/commonutilsdemo/test/TestDemo.kt
+++ app/src/main/java/com/hhl/commonutilsdemo/test/TestDemo.kt
@@ -8,8 +8,70 @@ package com.hhl.commonutilsdemo.test
 
 fun main() {
     println("--------welcome in main--------")
-    baseType()
-    arrayType()
+//    baseType()
+//    arrayType()
+    collectionTest()
+//    collectionSort()
+}
+
+/**
+ * 集合排序
+ */
+fun collectionSort() {
+    data class Language(var name: String, var score: Int)
+
+    val mutableList = mutableListOf<Language>();
+    mutableList.add(Language("JAVA", 80))
+    mutableList.add(Language("Kotlin", 90))
+    mutableList.add(Language("Android", 100))
+    mutableList.add(Language("C", 80))
+    println(mutableList)
+    //单条件排序
+    mutableList.sortBy { it.score }
+    println(mutableList)
+
+    //多条件排序
+    mutableList.sortWith(compareBy({ it.score }, { it.name }))
+    println(mutableList)
+
+}
+
+fun collectionTest() {
+    val list = listOf("A", "B", "A")
+    println("list=$list")
+    val set = setOf<String>("A", "B", "A")
+    println("set=$set")
+    val mutableList = mutableListOf<String>("A", "B")
+    mutableList.add("C")
+    mutableList.add("D")
+    mutableList.removeAt(0)
+    mutableList[0] = "A"
+    println("mutableList=$mutableList")
+    val mutableSet = mutableSetOf(1, 2, 3)
+    mutableSet.add(4)
+    println("mutableSet=$mutableSet")
+    //集合支持加减法
+    val tempSet = mutableSet + setOf(5, 6, 7)
+    println("tempSet=$tempSet")
+
+    val mutableMap = mutableMapOf<String, String>()
+    mutableMap["key1"] = "A"
+    mutableMap["key2"] = "B"
+    mutableMap["key3"] = "C"
+    mutableMap["key4"] = "D"
+    mutableMap["key5"] = "E"
+    println(mutableMap)
+    println("values=" + mutableMap.values)
+    println("keys=" + mutableMap.keys)
+    if (mutableMap.containsKey("key1")) {
+        println("mutableMap exist key1")
+    }
+
+    if ("A" in mutableMap.values) {
+        println("A in mutableMap")
+    }
+
+
 }
 
 fun arrayType() {
