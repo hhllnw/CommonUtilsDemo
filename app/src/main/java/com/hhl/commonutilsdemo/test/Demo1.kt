345d4b168551da180b1da37472f09d4e3d80b636
hhllnw <hhllnw@gmail.com>
2021-03-19 14:58:24 +0800

    kotlin基础练习

--- app/src/main/java/com/hhl/commonutilsdemo/test/Demo1.kt
+++ app/src/main/java/com/hhl/commonutilsdemo/test/Demo1.kt
@@ -1,7 +1,5 @@
 package com.hhl.commonutilsdemo.test
 
-import java.io.BufferedReader
-import java.io.Reader
 import java.lang.Exception
 import java.lang.IllegalArgumentException
 import java.util.*
